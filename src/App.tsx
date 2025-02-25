01d93f4c6d54204fd2906b9ec8d698f2cf465094
parvathyjnair <114852295+parvathyjnair@users.noreply.github.com>
2025-02-26 00:41:56 +0530

    refactor App component to use IonSplitPane and Menu for improved layout and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,46 @@
-import { IonApp, IonRouterOutlet, setupIonicReact, IonTabs, IonTabBar,
-  IonTabButton,  IonLabel, IonBadge, IonIcon } from '@ionic/react';
-import { calendar, personCircle, map, informationCircle } from 'ionicons/icons';
+import { IonApp, IonRouterOutlet, IonSplitPane, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
-import Page from "./pages/DashboardPage";
-import LandingPage from "./pages/LandingPage";
-import useStore from "./store";
+import Menu from './components/Menu';
+import Page from './pages/Page';
 
 /* Core CSS required for Ionic components to work properly */
-import "@ionic/react/css/core.css";
+import '@ionic/react/css/core.css';
 
 /* Basic CSS for apps built with Ionic */
-import "@ionic/react/css/normalize.css";
-import "@ionic/react/css/structure.css";
-import "@ionic/react/css/typography.css";
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
 
 /* Optional CSS utils that can be commented out */
-import "@ionic/react/css/padding.css";
-import "@ionic/react/css/float-elements.css";
-import "@ionic/react/css/text-alignment.css";
-import "@ionic/react/css/text-transformation.css";
-import "@ionic/react/css/flex-utils.css";
-import "@ionic/react/css/display.css";
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
 
 /* Theme variables */
-import "./theme/variables.css";
+import './theme/variables.css';
 
 setupIonicReact();
 
 const App: React.FC = () => {
-  const { isLoggedIn } = useStore();
   return (
     <IonApp>
-      <IonRouterOutlet id="main">
-        <IonReactRouter>
-          <Route path="/login" exact={true}>
-            <LandingPage />
-          </Route>
-          <Route path="/" exact={true}>
-            <Redirect to={isLoggedIn ? "/page/Home" : "/login"} />
-          </Route>
-          <Route path="/page/Home" exact={true}>
-            <Page tab="Home" />
-          </Route>
-          <Route path="/page/Activity" exact={true}>
-            <Page tab="Activity" />
-          </Route>
-          <Route path="/page/Settings" exact={true}>
-            <Page tab="Settings" />
-          </Route>
-        </IonReactRouter>
-      </IonRouterOutlet>
+      <IonReactRouter>
+        <IonSplitPane contentId="main">
+          <Menu />
+          <IonRouterOutlet id="main">
+            <Route path="/" exact={true}>
+              <Redirect to="/page/Inbox" />
+            </Route>
+            <Route path="/page/:name" exact={true}>
+              <Page />
+            </Route>
+          </IonRouterOutlet>
+        </IonSplitPane>
+      </IonReactRouter>
     </IonApp>
   );
 };
